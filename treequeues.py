c8f3d57d61639ef333f5ae8b7b7f03b59c3e191d
Thomas <hirtzt@gmail.com>
2022-04-14 22:22:42 +0200

    Clean code, add pickling capabilities, fix copy bug, and improve lock mechanics

--- treequeues.py
+++ treequeues.py
@@ -1,4 +1,6 @@
 import multiprocessing as mp
+from typing import Any
+from typing import Dict
 
 import numpy as np
 import tree  # noqa
@@ -13,71 +15,86 @@ class ArrayView:
             numpy_array: np.ndarray,
             num_items: int,
     ):
-        self._num_items = num_items
-        self._item_shape = numpy_array.shape
-
+        self.num_items = num_items
         self.dtype = numpy_array.dtype
         self.shape = (num_items, *numpy_array.shape)
+        self.nbytes: int = numpy_array.nbytes * num_items
 
-        self._view = np.frombuffer(
+        self._item_shape = numpy_array.shape
+        self._multiprocessing_array = multiprocessing_array
+
+        self._array_view = np.frombuffer(
             buffer=multiprocessing_array,
             dtype=numpy_array.dtype,
             count=np.product(self.shape),
         ).reshape(self.shape)
-        # todo maybe add a `set` to assert that the wrong items are not retrieved
 
-    def put(self, element: np.ndarray, index: int) -> None:
-        self._view[index, ...] = element
+    def __getstate__(self) -> Dict[str, Any]:
+        state = dict(self.__dict__)
+        del self.__dict__['_view']
+        return state
+
+    def __setstate__(self, state: Dict[str, Any]) -> None:
+        self.__dict__.update(state)
+        self._array_view = np.frombuffer(
+            buffer=self._multiprocessing_array,
+            dtype=self.dtype,
+            count=np.product(self.shape),
+        ).reshape(self.shape)
+
+    def put(self, item: np.ndarray, index: int) -> None:
+        assert item.shape == self._item_shape and item.dtype == self.dtype
+        self._array_view[index, ...] = item
 
     def get(self, index: int) -> np.ndarray:
-        return self._view[index, ...]
+        return np.copy(self._array_view[index, ...])
 
 
 class ArrayQueue:
     def __init__(self, array: np.ndarray, maxsize: int):
-        self.size = maxsize
-        self.num_bytes: int = array.nbytes * maxsize
-        self.array = mp.Array("c", self.num_bytes)
+        self.maxsize = maxsize
+        self.nbytes: int = array.nbytes * maxsize
 
-        self.queue = mp.Queue(maxsize=maxsize)
-        self.next_index = mp.Value('i', 0)
-        self.array_lock = mp.Lock()
+        self._array_lock = mp.Lock()
+        self._next_index = mp.Value('i', 0)
+        self._queue = mp.Queue(maxsize=maxsize)
+        self._array = mp.Array("c", self.nbytes)
 
-        self.view = ArrayView(
-            multiprocessing_array=self.array.get_obj(),
+        self._array_view = ArrayView(
+            multiprocessing_array=self._array.get_obj(),
             numpy_array=array,
             num_items=maxsize,
         )
 
-    def put(self, element: np.ndarray) -> None:
-        # Avoid putting several elements at the same time
-        with self.next_index.get_lock():
-            self.queue.put(self.next_index.value)
-            self._put(element=element, index=self.next_index.value)
-            self.next_index.value = (self.next_index.value + 1) % self.size
+    def put(self, item: np.ndarray) -> None:
+        # Avoid several simultaneous 'put' call
+        with self._next_index.get_lock():
+            self._queue.put(self._next_index.value)
+            # Avoid ArrayQueue changes during a 'put' or 'get' call
+            with self._array_lock:
+                self._put(item=item, index=self._next_index.value)
+            self._next_index.value = (self._next_index.value + 1) % self.maxsize
 
-    def _put(self, element: np.ndarray, index: int):
-        # Avoid array changes during putting and getting items
-        with self.array_lock:
-            self.view.put(element, index)
+    def _put(self, item: np.ndarray, index: int) -> None:
+        self._array_view.put(item, index)
 
     def get(self, block=True, timeout=None) -> np.ndarray:
-        index = self.queue.get(block=block, timeout=timeout)
-        with self.array_lock:
-            return self.view.get(index)
+        index = self._queue.get(block=block, timeout=timeout)
+        # Avoid ArrayQueue changes during a 'put' or 'get' call
+        with self._array_lock:
+            return self._get(index=index)
 
     def _get(self, index: int) -> np.ndarray:
-        with self.array_lock:
-            return self.view.get(index)
+        return self._array_view.get(index=index)
 
     def empty(self) -> bool:
-        return self.queue.empty()
+        return self._queue.empty()
 
     def full(self) -> bool:
-        return self.queue.full()
+        return self._queue.full()
 
     def qsize(self) -> int:
-        return self.queue.qsize()
+        return self._queue.qsize()
 
 
 class TreeQueue:
@@ -85,42 +102,47 @@ class TreeQueue:
             self, nested_array: NestedArray, maxsize: int
     ):
         self.maxsize = maxsize
-        self.nested_array = nested_array
+        self._nested_array = nested_array
 
-        self.queue = mp.Queue(maxsize=maxsize)
-        self.next_index = mp.Value('i', 0)
-        self.lock = mp.Lock()
+        self._lock = mp.Lock()
+        self._next_index = mp.Value('i', 0)
+        self._queue = mp.Queue(maxsize=maxsize)
 
         self._nested_queue = tree.map_structure(
             lambda array: ArrayQueue(array=array, maxsize=maxsize), nested_array
         )
 
+        self.nbytes = sum([q.nbytes for q in tree.flatten(self._nested_queue)])
+
+    def put(self, nested_array: NestedArray, block=True, timeout=None) -> None:
+        # Avoid several simultaneous 'put' call
+        with self._next_index.get_lock():
+            self._queue.put(self._next_index.value, block=block, timeout=timeout)
+            # Avoid ArrayQueue changes during a 'put' or 'get' call
+            with self._lock:
+                tree.map_structure(
+                    lambda queue, array: queue._put(  # noqa
+                        item=array,
+                        index=self._next_index.value
+                    ),
+                    self._nested_queue, nested_array
+                )
+            self._next_index.value = (self._next_index.value + 1) % self.maxsize
+
     def get(self, block=True, timeout=None) -> NestedArray:
-        index = self.queue.get(block=block, timeout=timeout)
-        with self.lock:
+        index = self._queue.get(block=block, timeout=timeout)
+        # Avoid ArrayQueue changes during a 'put' or 'get' call
+        with self._lock:
             return tree.map_structure(
                 lambda queue: queue._get(index=index),  # noqa
                 self._nested_queue
             )
 
-    def put(self, nested_array: NestedArray, block=True, timeout=None) -> None:
-        # Avoid putting several elements at the same time
-        with self.next_index.get_lock():
-            self.queue.put(self.next_index.value, block=block, timeout=timeout)
-            tree.map_structure(
-                lambda queue, array: queue._put(  # noqa
-                    element=array,
-                    index=self.next_index.value
-                ),
-                self._nested_queue, nested_array
-            )
-            self.next_index.value = (self.next_index.value + 1) % self.maxsize
-
     def empty(self) -> bool:
-        return self.queue.empty()
+        return self._queue.empty()
 
     def full(self) -> bool:
-        return self.queue.full()
+        return self._queue.full()
 
     def qsize(self) -> int:
-        return self.queue.qsize()
+        return self._queue.qsize()
